a2361bfa058e3c294e7ecf0783e89c490ac5d751
bulbasaur232 <rdd2033@naver.com>
2023-05-11 16:43:05 +0900

    [nft-trade] reserveCar()에서 차량이 거래중이면 revert 하도록 수정

--- contracts/nft-trade.sol
+++ contracts/nft-trade.sol
@@ -8,7 +8,7 @@ contract CarNFT_Trade is CarNFT_SaleRegistration, IKIP17Receiver{
     // 구매자가 클레이를 지불하여 구매 요청을 보내는 함수
     function reserveCar(uint _tokenId) external payable registeredForSale(_tokenId) correctState(_tokenId, Status.Registered) {
         // 이미 거래중이면 revert
-        require(isTrading(_tokenId) || msg.sender != _transactions[_tokenId].seller , "This car is currently being traded");
+        require(!isTrading(_tokenId) || msg.sender != _transactions[_tokenId].seller , "This car is currently being traded");
         // 보낸 클레이가 가격과 다르면 revert
         require(msg.value == _carDetails[_tokenId].price, "You must pay the correct price");
         
