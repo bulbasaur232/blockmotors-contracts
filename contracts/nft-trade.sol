9d48489d4693c3755dcd4cca654ea1c3bf84690b
bulbasaur232 <rdd2033@naver.com>
2023-06-04 21:48:05 +0900

    [nft-trade.sol] reserveCar()로 전송된 클레이를 컨트랙트로 보내는 코드 삭제

--- contracts/nft-trade.sol
+++ contracts/nft-trade.sol
@@ -5,31 +5,35 @@ import "./nft-sale-registration.sol";
 
 contract CarNFT_Trade is CarNFT_SaleRegistration, IKIP17Receiver{
 
-    // 구매자가 클레이를 지불하여 구매 요청을 보내는 함수
+    /**
+    * 구매자가 클레이를 지불하여 구매 요청을 보내는 함수
+    */
     function reserveCar(uint _tokenId) external payable registeredForSale(_tokenId) correctState(_tokenId, Status.Registered) {
-        // 이미 거래중이면 revert
         require(!isTrading(_tokenId) || msg.sender != _transactions[_tokenId].seller , "This car is currently being traded");
-        // 보낸 클레이가 가격과 다르면 revert
         require(msg.value == _carDetails[_tokenId].price, "You must pay the correct price");
         
+        /*
         (bool success, ) = payable(address(this)).call{value:msg.value}("");
         require(success, "Failed to send KLAY to Contract");
+        */
     
         _transactions[_tokenId].buyer = msg.sender;
         _transactions[_tokenId].state = Status.Reserved;
         _transactions[_tokenId].timestamp = block.timestamp;
     }
 
-    // 판매자가 NFT 판매 승인하는 함수
+    /**
+    * 판매자가 NFT 판매 승인하는 함수
+    */
     function confirmSelling(uint _tokenId) external onlyNFTOwner(_tokenId) registeredForSale(_tokenId) correctState(_tokenId, Status.Reserved) {
-        // NFT를 구매자에게 approve
         approve(_transactions[_tokenId].buyer, _tokenId);
         _transactions[_tokenId].state = Status.Approved;
     }
 
-    // 구매자가 최종 구매 승인하는 함수
+    /**
+    * 구매자가 최종 구매 승인하는 함수
+    */
     function confirmBuying(uint _tokenId) external registeredForSale(_tokenId) correctState(_tokenId, Status.Approved) returns (bool){
-        // msg.sender가 구매자가 아니면 revert 
         require(msg.sender == _transactions[_tokenId].buyer, "Caller is not the buyer of the car");
 
         // 구매 승인 기한 일주일 지나면 구매 취소
@@ -45,19 +49,16 @@ contract CarNFT_Trade is CarNFT_SaleRegistration, IKIP17Receiver{
     }
 
     function _completeTransaction(uint _tokenId) private {
-        // 트랜잭션들 정보수정
         _transactions[_tokenId].state = Status.Completed;
         _transactions[_tokenId].timestamp = block.timestamp;
         _prevTransactions[_tokenId].push(_transactions[_tokenId]);
         
-        // 클레이와 NFT 정산
         (bool success, ) = payable(_transactions[_tokenId].seller).call{value: _transactions[_tokenId].price}("");
         require(success, "Failed to send KLAY to buyer");
         safeTransferFrom(_transactions[_tokenId].seller, _transactions[_tokenId].buyer, _tokenId);
         
         emit transactionCompleted(block.timestamp, _tokenId, _transactions[_tokenId].seller, _transactions[_tokenId].buyer);
 
-        // 판매 목록에서 내리기
         _popOnSale(_tokenId);
     }
 
@@ -67,9 +68,7 @@ contract CarNFT_Trade is CarNFT_SaleRegistration, IKIP17Receiver{
     function cancelCarSale(uint _tokenId) public registeredForSale(_tokenId) {
         require(msg.sender == _transactions[_tokenId].seller, "Only the seller can cancel the sale");
 
-        // KLAY 환불
         _refund(_tokenId);
-        // 판매 목록에서 내리기
         _popOnSale(_tokenId);
     }
 
@@ -81,9 +80,7 @@ contract CarNFT_Trade is CarNFT_SaleRegistration, IKIP17Receiver{
         require(isTrading(_tokenId), "This car is not trading.");
         require(msg.sender == _transactions[_tokenId].buyer, "The caller is not the buyer.");
 
-        // KLAY 환불
         _refund(_tokenId);
-        // 차량의 상태를 Registered로 바꿈
         _transactions[_tokenId].timestamp = _carDetails[_tokenId].registDate;
         _transactions[_tokenId].buyer = address(0);
         _transactions[_tokenId].state = Status.Registered;
@@ -93,14 +90,15 @@ contract CarNFT_Trade is CarNFT_SaleRegistration, IKIP17Receiver{
     * 거래 취소 시 KLAY와 NFT를 돌려주는 함수
     */
     function _refund(uint _tokenId) private {
-        // 구매자에게 KLAY 환불
         if(isTrading(_tokenId)){
             (bool success, ) = payable(_transactions[_tokenId].buyer).call{value: _transactions[_tokenId].price}("");
             require(success, "Failed to send KLAY to buyer");
         }
     }
 
-    // 차량을 판매 목록에서 제거하는 함수
+    /** 
+    * 차량을 판매 목록에서 제거하는 함수
+    */
     function _popOnSale(uint _tokenId) private {
         require(_carsOnSale.length > 0, "No cars for sale");
         uint idx = 0;
@@ -115,7 +113,6 @@ contract CarNFT_Trade is CarNFT_SaleRegistration, IKIP17Receiver{
         _carsOnSale[idx] = _carsOnSale[_carsOnSale.length - 1];
         _carsOnSale.pop();
 
-        // Detail과 Transation 삭제
         delete _carDetails[_tokenId];
         delete _transactions[_tokenId];
     }
